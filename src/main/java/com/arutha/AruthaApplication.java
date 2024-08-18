5ddd4d15621df721329940ced1fac0afe8a3cdb3
ushandilusha <ushandilusha10@gmail.com>
2024-08-18 22:27:47 +0530

    base configuration and DB configuration

---
+++ src/main/java/com/arutha/AruthaApplication.java
@@ -0,0 +1,15 @@
+package com.arutha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AruthaApplication {
+
+	public static void main(String[] args) {
+
+		System.setProperty("server.servlet.context-path", "/api");
+		SpringApplication.run(AruthaApplication.class, args);
+	}
+
+}
